cd70fe81c0276985d06225baab67e77eeac628cc
root <root@localhost>
2024-01-30 04:45:27 +0000

    book collection 1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,56 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def new
+    @book = Book.new
+  end
+
+
+
+
+  def book_params
+    params.require(:book).permit(:title)
+  end
+
+  def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+  
+    if @book.update(book_params)
+      flash[:notice] = 'Book was successfully updated.'
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+  
+  def delete
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:notice] = 'Book was successfully deleted.'
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
-  root "main#index"
-  
-  get 'main/index'
-  get 'books/index'
-  get 'books/new'
-  get 'books/edit'
+  root "books#index"
+  resources :books
+
+  resources :books do
+    member do 
+      get :delete
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
